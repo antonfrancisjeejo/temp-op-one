ee1fea8c14c5e2df16938c1d022a4a9c7a7f0e15
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2021-10-20 04:28:13 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+var a = "Jackson"; // any words, letter or sentence
+var b = 10; // -infinity to +infinity
+var c = true; // false
+
+// if age greater than 18 eligible to vote or not eligble
+// var age = 15;
+// if (age > 18) {
+//     console.log("You are eligible to vote");
+//   } else {
+//     console.log("You are not eligible to vote");
+//   }
+
+//if age is inbetween 10 to 18 then they get 3 choclates if their age is between 19 to 50
+// they get one choclate and if age greater than 60 they get zero
+// var age = 55;
+// if (age > 9 && age <= 18) {
+//   console.log("You get three");
+// } else if (age >= 19 && age <= 50) {
+//   console.log("You get one");
+// } else {
+//   console.log("You get zero");
+// }
+
+// var age = 15;
+// var grade = "B";
+
+// if (age > 10 && grade == "A") {
+//   console.log("You are good");
+// }
+// if (age > 20 || grade === "A") {
+//   console.log("You are okay");
+// } else {
+//   console.log("You should check");
+// }
+
+// === - Strictly equal
+
+// var isAvailable = 10;
+
+// if (isAvailable === "10") {
+//   console.log("It is available");
+// }
+// 0        1         2
+var fruits = ["apple", "mango", "orange"];
+
+for (var i = 0; i < 3; i++) {
+  console.log(fruits[i]);
+}
